49e503cec580c1356a262cba8318c5e9516c30c4
Scott Mitchell <email4scottmitchell@gmail.com>
2016-05-05 17:05:43 +0000

    finish FlickList 3 studio

--- js/flicklist.js
+++ js/flicklist.js
@@ -3,13 +3,13 @@
 var model = {
   watchlistItems: [],
   browseItems: []
-}
+};
 
 
 var api = {
 
   root: "https://api.themoviedb.org/3",
-  token: "TODO", // TODO 0 add your api key
+  token: "60f47effe64bc8f896debedd3c546a64", // TODO 0 add your api key
 
   /**
    * Given a movie object, returns the url to its poster image
@@ -17,10 +17,12 @@ var api = {
   posterUrl: function(movie) {
     // TODO 4b
     // implement this function
+    var baseImageUrl = "http://image.tmdb.org/t/p/w300/";
+    return baseImageUrl + movie.poster_path;
 
-    return "http://images5.fanpop.com/image/photos/25100000/movie-poster-rapunzel-and-eugene-25184488-300-450.jpg" 
+    //return "http://images5.fanpop.com/image/photos/25100000/movie-poster-rapunzel-and-eugene-25184488-300-450.jpg"; 
   }
-}
+};
 
 
 /**
@@ -76,11 +78,35 @@ function render() {
 
   // insert watchlist items
   model.watchlistItems.forEach(function(movie) {
-    var title = $("<h6></h6>").text(movie.original_title);
+    var title = $("<h6></h6>")
+    .text(movie.original_title)
+    .attr("class", "panel-heading");
+    
+    var panelHeading = $("<div></div>")
+    .attr("class", "panel-heading")
+    .append(title);
 
     // TODO 1 
     // add an "I watched it" button and append it below the title
     // Clicking should remove this movie from the watchlist and re-render
+    
+    var button = $("<button></button>")
+    .text("I watched it")
+    .attr("class", "btn btn-danger")
+    .click(function(){
+      var index = model.watchlistItems.indexOf(movie);
+      model.watchlistItems.splice(index, 1);
+      render();
+    });
+    
+    var poster = $("<img></img>").attr("src", api.posterUrl(movie))
+    .attr("class", "img-responsive");
+    
+    var panelBody = $("<div></div>")
+    .attr("class", "panel-body")
+    .append(poster)
+    .append(button);
+    
 
     // TODO 2i
     // apply the classes "btn btn-danger" to the "I watched it button"
@@ -88,12 +114,15 @@ function render() {
     // TODO 4a
     // add a poster image and append it inside the 
     // panel body above the button
+    
+    
 
     // TODO 2g
     // re-implement the li as a bootstrap panel with a heading and a body
     var itemView = $("<li></li>")
       .append(title)
-      .attr("class", "item-watchlist");
+      .append(button)
+      .attr("class", "panel panel-default");
 
     $("#section-watchlist ul").append(itemView);
   });
