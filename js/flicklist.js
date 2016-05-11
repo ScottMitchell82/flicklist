a00198787361977ebbae3a393011b6f749f75518
Scott Mitchell <email4scottmitchell@gmail.com>
2016-05-11 00:19:19 +0000

    Finish Flicklist4 studio

--- js/flicklist.js
+++ js/flicklist.js
@@ -8,7 +8,7 @@ var model = {
 
 var api = {
   root: "https://api.themoviedb.org/3",
-  token: "TODO", // TODO 0 add your api key
+  token: "60f47effe64bc8f896debedd3c546a64", // TODO 0 add your api key
   /**
    * Given a movie object, returns the url to its poster image
    */
@@ -28,7 +28,7 @@ var api = {
 
 // TODO 1
 // this function should accept a second argument, `keywords`
-function discoverMovies(callback) {
+function discoverMovies(callback, keywords) {
 
   // TODO 2 
   // ask the API for movies related to the keywords that were passed in above
@@ -38,6 +38,7 @@ function discoverMovies(callback) {
     url: api.root + "/discover/movie",
     data: {
       api_key: api.token,
+      with_keywords: keywords
     },
     success: function(response) {
       model.browseItems = response.results;
@@ -91,14 +92,23 @@ function searchMovies(query, callback) {
 
 
   $.ajax({
-    url: api.root + "/search/movie",
+    url: api.root + "/search/keyword",
     data: {
       api_key: api.token,
       query: query
     },
     success: function(response) {
       console.log(response);
+      var keywordIDs = response.results.map(function(keywordObj) {
+        var id = keywordObj.id;
+        return id;
+      });
+      console.log(keywordIDs);
+      var keywordsString = keywordIDs.join([separator = '|']);
+      console.log(keywordsString);
+      discoverMovies(callback, keywordsString);
     }
+    
   });
 }
 
